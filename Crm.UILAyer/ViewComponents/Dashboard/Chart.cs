9078e2703823a600ccb33fb4b573d5d8d6c2beae
Songül YAMAN BAYIK <snglymn@gmail.com>
2022-10-04 22:27:40 +0300

    ders 29

---
+++ Crm.UILAyer/ViewComponents/Dashboard/Chart.cs
@@ -0,0 +1,23 @@
+﻿using Crm.DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crm.UILAyer.ViewComponents.Dashboard
+{
+    public class Chart: ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.P1 = context.Products.Where(x => x.ProductID == 1).Select(y => y.ProductStock).FirstOrDefault();
+            ViewBag.P2 = context.Products.Where(x => x.ProductID == 2).Select(y => y.ProductStock).FirstOrDefault();
+            ViewBag.P3 = context.Products.Where(x => x.ProductID == 3).Select(y => y.ProductStock).FirstOrDefault();
+            ViewBag.P4 = context.Products.Where(x => x.ProductID == 8).Select(y => y.ProductStock).FirstOrDefault();
+            ViewBag.P5 = context.Products.Where(x => x.ProductID == 9).Select(y => y.ProductStock).FirstOrDefault();
+            return View();
+        }
+    }
+}
---
+++ Crm.UILAyer/ViewComponents/Dashboard/Widgets.cs
@@ -0,0 +1,22 @@
+﻿using Crm.DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crm.UILAyer.ViewComponents.Dashboard
+{
+    public class Widgets : ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v = context.Categories.Count();
+            ViewBag.v2 = context.Products.Count();
+
+            return View();
+
+        }
+    }
+}
